bc280321465ddd0bda3ddbbeb4451407be5078cf
tarasfrompir <tarasfrompir@inbox.ru>
2017-07-20 19:44:15 +0300

    Перевод на украинский язык
    СПС за просмотр

---
+++ languages/scheduler_ua.php
@@ -0,0 +1,10 @@
+<?php
+$dictionary=array(
+'SCHEDULER_MODULE_TITLE'=>'Планувальник завдань',
+/* end module names */
+);
+foreach ($dictionary as $k=>$v) {
+ if (!defined('LANG_'.$k)) {
+  define('LANG_'.$k, $v);
+ }
+}
